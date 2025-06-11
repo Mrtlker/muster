635645d60e8c420b856ab6b19c68d3499b3f44e6
Mrtlker <ulkermert7@gmail.com>
2025-06-11 16:15:54 +0300

    Add files via upload

---
+++ MusteriTakip_ReadyToUpload_GitHub (1)/src/main.jsx
@@ -0,0 +1,9 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1 style={{ color: 'red' }}>Müşteri Takip Uygulaması Başladı</h1>
+  </React.StrictMode>,
+)
