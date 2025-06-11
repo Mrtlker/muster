9c9820690f53b60c9487c7c99e126cee73b3773c
Mrtlker <ulkermert7@gmail.com>
2025-06-11 16:38:17 +0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1 style={{ color: "red" }}>Müşteri Takip Uygulaması Aktif</h1>
-  </React.StrictMode>
-);
+    <h1 style={{ color: 'red' }}>Müşteri Takip Uygulaması Başladı</h1>
+  </React.StrictMode>,
+)
